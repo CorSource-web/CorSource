9a6d4c152324f5f8facd7cc043df0d463240c8e4
Jay Winebrenner <jaywinebrenner@gmail.com>
2025-09-22 14:49:04 -0700

    tweak footer stylings

--- footer.php
+++ footer.php
@@ -31,9 +31,8 @@
 							<p class="minor bold">Job Seekers</p>
 						</a>
 						<div class="social-wrapper">
-							<a target="_blank" href="https://www.linkedin.com/company/corsource-technology-group/"><img class="social" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/footer/linkedin.svg" /></a>
-							<a target="_blank" href="https://x.com/CorSource"><img class="social" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/footer/twitter.svg" /></a>
-							<a target="_blank" href="https://www.facebook.com/CorSourceTech/"><img class="social" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/footer/facebook.svg" /></a>
+			
+							<a target="_blank" href="https://www.facebook.com/CorSourceTech/"><img class="social" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/footer/facebook-white.png" /></a>
 						</div>
 
 					</div>
